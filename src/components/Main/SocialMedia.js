dba31518e28d73897cdf3962ab4596c81c0cfbb2
Hamed Osama <hamedosama19@gmail.com>
2022-08-14 21:51:54 +0200

    main style updated

--- src/components/Main/SocialMedia.js
+++ src/components/Main/SocialMedia.js
@@ -2,25 +2,27 @@ import React from 'react'
 
 import style from './SocialMedia.module.css'
 import instagram from '../../assets/owner/instagram.png'
-import twitter from '../../assets/owner/twitter.png'
+import github from '../../assets/owner/github.png'
 import more from '../../assets/owner/more.png'
 function SocialMedia() {
   return (
     <ul className={style.socialMediaContainer}>
       <li className={style.socialMedia}>
-        <a href="#1" className={style.link}>
-          <img src={instagram} alt="instagram" />
+        <a href="https://www.instagram.com/hamed_prvv/" rel='noreferrer' target={'_blank'} className={style.link}>
+          <img src={instagram} alt="Instagram" />
         </a>
       </li>
       <li className={style.socialMedia}>
-        <a href="#1" className={style.link}>
-          <img src={twitter} alt="instagram" />
+        <a href="https://github.com/HamedOsama" rel='noreferrer' target={'_blank'} className={style.link}>
+          <img src={github} alt="Github" className={style.invert} />
         </a>
       </li>
       <li className={style.socialMedia}>
-        <a href="#1" className={style.link}>
-          <img src={more} alt="instagram" />
-        </a>
+        {/* <a href="##" className={style.link}> */}
+        <div className={style.link}>
+          <img src={more} alt="More" />
+        </div>
+        {/* </a> */}
       </li>
     </ul>
   )
