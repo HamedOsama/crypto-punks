78362a120683079bdf34eda53b71eda5d7997566
Hamed Osama <hamedosama19@gmail.com>
2022-08-15 02:20:32 +0200

    usecontext added

--- src/components/Header/HeaderActions.js
+++ src/components/Header/HeaderActions.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 
 import style from './HeaderActions.module.css'
 import theme from '../../assets/header/theme-switch.png'
+import ThemeContext from '../../store/theme-context'
 const HeaderActions = () => {
-  const [currentTheme, setCurrentTheme] = useState('dark');
+  const themeCtx = useContext(ThemeContext);
+  // const [currentTheme, setCurrentTheme] = useState('dark');
   // useEffect(() => {
   //   document.documentElement.style.setProperty('--main-color', `${currentTheme === 'dark' ? '#fff' : '#000'}`)
   //   document.documentElement.style.setProperty('--sec-color', `${currentTheme === 'dark' ? '#a1a5b0' : '#1d1d1d'}`)
@@ -12,15 +14,15 @@ const HeaderActions = () => {
   //   document.documentElement.style.setProperty('--input-color', `${currentTheme === 'dark' ? '#eee' : '#1d1d1d'}`)
 
   // }, [currentTheme]);
-  const changeThemeHandler = () => {
-    document.lastChild.dataset.theme = `${currentTheme === 'dark' ? 'light' : 'dark'}`
-    setCurrentTheme(`${currentTheme === 'dark' ? 'light' : 'dark'}`)
-  }
+  // const changeThemeHandler = () => {
+  //   document.lastChild.dataset.theme = `${currentTheme === 'dark' ? 'light' : 'dark'}`
+  //   setCurrentTheme(`${currentTheme === 'dark' ? 'light' : 'dark'}`)
+  // }
   return (
-    <div className={style.HeaderActions} onClick={changeThemeHandler}>
+    <div className={style.HeaderActions} onClick={themeCtx.changeThemeHandler}>
       <div className={style.themeContainer}>
         <img src={theme} alt="Theme"
-          className={`${style.theme} ${currentTheme === 'light' ? 'light' : ''} 
+          className={`${style.theme} ${themeCtx.currentTheme === 'light' ? 'light' : ''} 
         `} />
       </div>
       <button className={style.primaryBtn}>Get In</button>
